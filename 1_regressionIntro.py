c4726044532d5bd56ae80b22c33de6a7d2d3376e
vrunal <vrunal1994@gmail.com>
2018-01-14 00:21:41 -0500

    First Commit with 3 exercises

---
+++ 1_regressionIntro.py
@@ -0,0 +1,14 @@
+# To take continous data, find the equation 
+# that best fits the data and be able to forecast out a 
+# specific value. Regression is form of Supervised ML
+
+
+import pandas as pd
+import quandl
+
+df = quandl.get("WIKI/GOOGL")
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+print(df.tail())
